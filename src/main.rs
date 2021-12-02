cc57f1f6449e52f58f4c3c8f2ff232f15ba423ac
Adam Lefevre <adamtlefevre@gmail.com>
2021-12-02 13:06:15 -0600

    day 2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod utils;
 use std::{env, error::Error};
 use utils::read_line_delimited;
 
+
 fn main() -> Result<(), Box<dyn Error + 'static>>{
     
     let args = env::args().collect::<Vec<String>>();
@@ -13,8 +14,15 @@ fn main() -> Result<(), Box<dyn Error + 'static>>{
             let mut increases = count_windowed_increases(1, "p1.txt");
             println!("Found {} increases", increases);
 
-            increases = count_windowed_increases(3, "p1_2.txt");
+            increases = count_windowed_increases(3, "p1.txt");
             println!("Found {} Summed increases", increases)
+        }
+        2 => {
+            let mut product = find_current_position(false);
+            println!("Found product of distance and depth: {}", product);
+
+            product = find_current_position(true);
+            println!("Found product of distance and depth with aim: {}", product)
         }
         _ => println!("Problem {} not implemnted yet", problem)
     };
@@ -69,4 +77,44 @@ fn count_windowed_increases(window_size: usize, file_name: &str) -> u32 {
             }
         })
         
+}
+// Find the product of the distance and deph given the
+// input file
+//
+// Args:
+//      use_aim: A boolean indicating whether "aim" should be used
+//
+// Returns:
+//      The product of the distance traveled and the depth reached by the submarine
+//
+fn find_current_position(use_aim: bool) -> u64 {
+    let inputs = read_line_delimited("p2.txt");
+    
+    let mut depth = 0;
+    let mut distance = 0;
+    let mut aim = 0;
+    
+    for line in inputs {
+        let parts: Vec<_> = line.split_whitespace().collect();
+        let value = parts[1].parse::<u64>().unwrap();
+        let direction = parts[0];
+
+        match direction {
+            "forward" => {
+                if use_aim {
+                    depth += aim * value
+                }
+                distance += value;
+            },
+            "down" => aim += value,
+            "up" => aim -= value,
+            _ => unreachable!()
+        }
+    }
+    
+    if use_aim {
+        distance * depth
+    } else {
+        distance * aim
+    }
 }
